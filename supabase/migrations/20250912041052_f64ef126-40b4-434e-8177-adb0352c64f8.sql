5ea4f20f111ca33f2ccfaac099879f371189df9f
hein-01 <142292126+hein-01@users.noreply.github.com>
2025-09-18 10:45:45 +0000

    Delete all files and folders

--- supabase/migrations/20250912041052_f64ef126-40b4-434e-8177-adb0352c64f8.sql
+++
@@ -1,6 +0,0 @@
--- Rename columns in businesses table to match locations table naming
-ALTER TABLE businesses 
-RENAME COLUMN city TO towns;
-
-ALTER TABLE businesses 
-RENAME COLUMN state TO province_district;
